a51f092e88a535fea6e1ea620d1fd624a7c73f6b
Eliot Howes <ejhowes@gmail.com>
2019-07-05 10:44:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,14 +41,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-
-  for (let i=0; i<cart.length; i++) {
+  for (var i = 0; i <cart.length; i++) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1)
       return cart
-    }
+    } 
   }
-  console.log("That item is not in your cart.")
+  console.log('That item is not in your cart.')
   return cart
 }
 
