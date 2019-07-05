40493386ffd6b25504f749596ec71038d5b614e9
Eliot Howes <ejhowes@gmail.com>
2019-07-05 10:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (i=0; i<cart.length; i++) {
+  for (let i=0; i<cart.length; i++) {
     if (cart.include(item)) {
       cart.splice(cart.indexOf(item), 1)
       return cart
