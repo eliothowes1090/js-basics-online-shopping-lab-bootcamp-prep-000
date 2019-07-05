a6bcfdf75e567f66395766ee3ec13f73df8df67d
Eliot Howes <ejhowes@gmail.com>
2019-07-05 08:57:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ function viewCart() {
   for (let i=0; i < cart.length; i++) {
     if (cart.length == 0) {
       printCart.push("Your shopping cart is empty.");
-    } else if (i == cart.length - 1) {
-      printCart.push(`${cart[i].itemName} at $${cart[i].itemPrice}.`)
-    }
+    } 
+    // else if (i == cart.length - 1) {
+    //   printCart.push(`${cart[i].itemName} at $${cart[i].itemPrice}.`)
+    // }
   }
   return printCart
 }
